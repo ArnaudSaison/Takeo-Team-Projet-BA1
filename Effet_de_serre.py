e6801cfe31de77cf3ce3ff6d88fbd7c067e72eab
ArnaudS <44066592+ArnaudSaison@users.noreply.github.com>
2018-11-15 16:14:07 +0100

    Add files via upload

--- Effet_de_serre.py
+++ Effet_de_serre.py
@@ -1,55 +1,46 @@
+"""
+Bloc effet de serre
+Team Takeo
+"""
+
 import numpy as np
 import scipy.optimize as sp_op
 import scipy.constants as cste
 
-L = 0.8  # Dimensions de la boîte
-g = 9.80665  # accélération gravifique
-sigma = cste.sigma
-h = 4  # Valeur test
+L = 0.8             # Dimensions de la boîte
+g = 9.80665         # accélération gravifique
+sigma = cste.sigma  # constante de Stefan Boltzmann pour équation corps noir
 
-# Constantes :   sigma, v,        alpha,    Pr,    g, L, k,       h
-constantes300 = [sigma, 1.578e-5, 2.213e-5, 0.713, g, L, 0.02623, h]
-constantes310 = [sigma, 1.659e-5, 2.340e-5, 0.709, g, L, 0.02684, h]
-constantes320 = [sigma, 1.754e-5, 2.476e-5, 0.708, g, L, 0.02753, h]
-constantes330 = [sigma, 1.851e-5, 2.616e-5, 0.708, g, L, 0.02821, h]
-constantes340 = [sigma, 1.951e-5, 2.821e-5, 0.707, g, L, 0.02888, h]
+# Constantes :   sigma, g, v,        alpha,    Pr,    k,
+constantes300 = (sigma, g, 1.578e-5, 2.213e-5, 0.713, 0.02623)
+constantes310 = (sigma, g, 1.659e-5, 2.340e-5, 0.709, 0.02684)
+constantes320 = (sigma, g, 1.754e-5, 2.476e-5, 0.708, 0.02753)
+constantes330 = (sigma, g, 1.851e-5, 2.616e-5, 0.708, 0.02821)
+constantes340 = (sigma, g, 1.951e-5, 2.821e-5, 0.707, 0.02888)
 
 
-def Bloc_effet_de_serre(T, Fd, Fi, c):
+def Bloc_effet_de_serre(T, Fd, Fi, c=constantes320, L=1):
     """
     Caclule la puissance du capteur solaire en W/m^2.
 
     :param T: Température que l'on veut atteindre dans la serre
     :param Fd: Flux solaire direct
     :param Fi: Flux solaire indirect
-    :param c: constantes : sigma, v, alpha, Pr, g, L, k, h
+    :param c: constantes : sigma, g, v, alpha, Pr, k
+    :param L: périmètre de la section de la boîte
     :return P: Puissance développée par la serre
     """
-    # Constantes convection
-    sigma = c[0]
-    v = c[1]
-    alpha = c[2]
-    Pr = c[3]
-    g = c[4]
-    L = c[5]
-    k = c[6]
-    beta = (1 / T)
 
-    # h = c[7]
+    # Constantes convection
+    sigma, g, v, alpha, Pr, k, beta = c[0], c[1], c[2], c[3], c[4], c[5], (1 / T)
 
     # Définition du système d'équations
     def sys(x):
         # Inconnues
-        P = x[0]
-        Ts = x[1]
-        Tp = x[2]
-        Fs = x[3]
-        Fp = x[4]
+        P, Ts, Tp, Fs, Fp = x[0], x[1], x[2], x[3], x[4]
 
         # Inconnues convection
-        Ra = x[5]
-        Nu = x[6]
-        h = x[7]
+        Ra, Nu, h = x[5], x[6], x[7]
 
         # Vecteur qui va contenir les équations
         E = np.empty(8)
@@ -63,10 +54,8 @@ def Bloc_effet_de_serre(T, Fd, Fi, c):
 
         # Equations de a convection
         E[5] = (h * L / k) - Nu
-        E[6] = ((g * beta * (((Ts + Tp) / 2) - T) * L ** 3) / (alpha * v)) - Ra
-        # E[7] = 0.58 * Ra ** (1 / 5) - Nu
+        E[6] = ((g * beta * (((Tp + Ts) / 2) - T) * L ** 3) / (alpha * v)) - Ra
         E[7] = (0.14 * (Ra ** (1 / 3)) * ((1 + 0.0107 * Pr) / (1 + 0.01 * Pr))) - Nu
-        # E[7] = (0.58 * (Ra ** (1 / 5))) - Nu
 
         return E
 
@@ -77,6 +66,9 @@ def Bloc_effet_de_serre(T, Fd, Fi, c):
     sol = sp_op.root(sys, x0)
 
     return sol.x
+
+
+# Partie de test :
 
 
 def imprimer(T, Fd, Fi, constantes):
@@ -128,6 +120,3 @@ def main(mode):
         test(constantes330)
     else:
         print("Erreur")
-
-
-main(input())
