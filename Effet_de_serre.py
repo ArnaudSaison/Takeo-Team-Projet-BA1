0f936d3c9465fc0d867c3f12380dd2dfc59f32fe
ArnaudS <44066592+ArnaudSaison@users.noreply.github.com>
2018-11-24 12:27:54 +0100

    Update Effet_de_serre.py

--- Effet_de_serre.py
+++ Effet_de_serre.py
@@ -1,13 +1,27 @@
 """
-Bloc effet de serre
-Team Takeo
+Takeo
+
+CODE DU BLOC EFFET DE SERRE
+
+     grandeur                                               symbole [unités]
+Les inputs :
+   - Température que l'on veut atteindre dans la serre      T       [C°]
+   - Flux solaire direct                                    Fd      [W/m²]
+   - Flux solaire indirect                                  Fi      [W/m²]
+   - constantes : sigma, g, v, alpha, Pr, k                 c       (facultatif)
+   - longeur section                                        a       [m]
+   - hauteur de la section                                  b       [m]
+
+Les outputs :
+   - (list)     Puissance développée par la serre :         Fd      [W/m²]
+   P, Ts, Tp, Fs, Fp (puissance, température au sol, température du plastique, rayonnement du sol, rayonnement du plastique)
 """
 
 import numpy as np
 import scipy.optimize as sp_op
 import scipy.constants as cste
 
-g = cste.g         # accélération gravifique
+g = cste.g          # accélération gravifique
 sigma = cste.sigma  # constante de Stefan Boltzmann pour équation corps noir
 
 # Constantes :   sigma, g, v,        alpha,    Pr,    k,
@@ -18,18 +32,9 @@ constantes330 = (sigma, g, 1.851e-5, 2.616e-5, 0.708, 0.02821)
 constantes340 = (sigma, g, 1.951e-5, 2.821e-5, 0.707, 0.02888)
 
 
-def Bloc_effet_de_serre(T, Fd, Fi, a, b, c=constantes320):
+def Bloc_effet_de_serre(T, Fd, Fi, a, b, c=constantes340):
     """
     Caclule la puissance du capteur solaire en W/m^2.
-
-    :param T: Température que l'on veut atteindre dans la serre
-    :param Fd: Flux solaire direct
-    :param Fi: Flux solaire indirect
-    :param c: constantes : sigma, g, v, alpha, Pr, k
-    :param L: périmètre de la section de la boîte
-    :param a: longeur section
-    :param b:
-    :return P: Puissance développée par la serre
     """
 
     # Constantes convection
@@ -72,62 +77,3 @@ def Bloc_effet_de_serre(T, Fd, Fi, a, b, c=constantes320):
 
     return sol.x
 
-"""
-##################################################################################################################
-
-PARTIE TEST
-
-##################################################################################################################
-"""
-# Partie de test :
-
-
-def imprimer(T, Fd, Fi, constantes):
-    # Print de la puissance de cette serre
-    res = Bloc_effet_de_serre(T, Fd, Fi, 0.4, 0.02, constantes)
-    print("P  =\t", res[0],
-          "\nTs =\t", res[1],
-          "\nTp =\t", res[2],
-          "\nFs =\t", res[3],
-          "\nFp =\t", res[4],
-          "\nRa =\t", res[5],
-          "\nNu =\t", res[6],
-          "\nh  =\t", res[7], )
-    # print(res)
-
-
-def test2(constantes):
-    T = 65 + 273.15
-    Fd = 400
-    Fi = 400
-
-    imprimer(T, Fd, Fi, constantes)
-
-
-def test(constantes):
-    # variables d'entrée
-    T = float(input("T (Celsuis) = ")) + 273.15
-    print("T en Kelvin =", T)
-    Fd = float(input("Fd (Watts par mètre carré)= "))
-    Fi = float(input("Fi (Watts par mètre carré)= "))
-
-    imprimer(T, Fd, Fi, constantes)
-
-
-def main(mode):
-    print("\n", "#" * 50, "\n", "#" * 50, "\n")
-
-    if mode == "test":
-        test2(constantes300)
-        print("\n", "_" * 50, "\n")
-        test2(constantes310)
-        print("\n", "_" * 50, "\n")
-        test2(constantes320)
-        print("\n", "_" * 50, "\n")
-        test2(constantes330)
-        print("\n", "_" * 50, "\n")
-        test2(constantes340)
-    elif mode == "input":
-        test(constantes330)
-    else:
-        print("Erreur")
