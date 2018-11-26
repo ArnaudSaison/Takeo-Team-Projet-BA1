59036b580b3c4489381f13feb4e82c9f7f94058b
ArnaudS <44066592+ArnaudSaison@users.noreply.github.com>
2018-11-26 23:05:38 +0100

    Update Ventilation.py

--- Ventilation.py
+++ Ventilation.py
@@ -16,21 +16,24 @@ Les outputs :
    - débit d'air sec                             Q                  [kg d'air sec/s]
 """
 
+
 def Bloc_ventilation(Masse_aliment, Masse_epmsi, Masse_epmsf, Yamb, Ymax, Dt=16):
     """
     Ce programme nous permet de trouver le débit d'air nécessaire à notre séchoir
     Ce débit d'air est très important, car il va réguler l'humidité de la machine.
     """
     # Données
-    Masse_init_eau = Masse_epmsi * Masse_aliment
-    Masse_fin_eau = Masse_epmsf * Masse_aliment
+    Masse_init_eau = Masse_epmsi / (Masse_epmsi + 1) * Masse_aliment
+    Masse_fin_eau = Masse_epmsf / (Masse_epmsi + 1) * Masse_aliment
+
     D_masse = Masse_init_eau - Masse_fin_eau
     Dt = Dt * 3600  # Conversion du temps en secondes
 
     # Equations permettant de calculer par la suite le débit d'air sec
-    J = (D_masse) / Dt
-    Dy = Ymax - Yamb
+    J = D_masse / Dt
+    Dy = Yamb - Ymax
 
+    # Calcul du débit d'air
     Q = J / Dy
 
     return Q
